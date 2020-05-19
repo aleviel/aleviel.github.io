635b789a4426d53662272995e4b6e3bdcc33860b
Alex <alex666alex@Mail.ru>
2020-05-19 21:05:58 +0300

    lesson_26

---
+++ module_7-lesson_26/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include "_head.php"?>
+
+    <body>
+    
+        <!-- Loader -->
+    	<?php include "_loader.php"?>
+
+		<!-- navbar -->
+        <?php include "navbar.php"?>
+		
+        <!-- Page title -->
+        <div class="page-title top-content">
+            <div class="page-title-text wow fadeInUp">
+            	<h1>Welcome to our site</h1>
+            	<p>We are Lancar, a design agency doing cool things with Photoshop and WordPress. Take a look around to see what we can do for you!</p>
+            	<div class="page-title-bottom-link">
+            		<a class="big-link-1 btn" href="prices.php">Start now</a>
+            		<a class="big-link-2 btn" href="services.php">Learn more</a>
+            	</div>
+            </div>
+        </div>
+
+
+        <!-- Footer -->
+		<?php include "footer.php"?>
+
+        
+        <!-- Javascript -->
+        <?php include "_js.php"?>
+
+    </body>
+
+</html>
